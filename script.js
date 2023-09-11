ae01000f1f8380fdc2b1b9fbc23fdce20cade8de
Maurice Roach <mroach1993@hotmail.com>
2023-09-11 19:05:32 -0400

    added

---
+++ script.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector(".btn");
+const results = document.querySelector(".results");
+const container = document.querySelector(".container");
+
+let newItem = () => {
+  setTimeout(() => {
+    fetch("https://api.api-ninjas.com/v1/facts?limit=1", {
+      headers: { "X-Api-Key": "xIEFntgxDtuFjzfICe1x7g==z6wQuXIRLKePetzG" },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        results.innerHTML = data[0].fact;
+      });
+  }, 500);
+};
+
+btn.addEventListener("click", () => {
+  results.innerHTML = "Loading a new fact ...";
+  container.style.height = "350px";
+  container.style.width = "600px";
+  newItem();
+});
